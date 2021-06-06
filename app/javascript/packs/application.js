67dc8a555dab2613360e2e59cf08eb6d9eaabd6e
alvindcaesar <me@alvindcaesar.com>
2021-06-06 17:22:05 +0800

    Commits for 'feature/create-product' branch

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,24 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("local-time").start()
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
-import "stylesheets/application"
+window.Rails = Rails
+
+import 'bootstrap'
+
+document.addEventListener("turbolinks:load", () => {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+})
