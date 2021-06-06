5d14687300856a37515d2bec00d533b07bb2d2fa
alvindcaesar <me@alvindcaesar.com>
2021-06-06 17:25:22 +0800

    Commits for 'feature/create-product' branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-    authenticate :user, lambda { |u| u.admin? } do
-      mount Sidekiq::Web => '/sidekiq'
-    end
-
-
-  devise_for :users
-  root to: 'home#index'
   resources :products
+  get '/privacy', to: 'home#privacy'
+  get '/terms', to: 'home#terms'
+authenticate :user, lambda { |u| u.admin? } do
+  mount Sidekiq::Web => '/sidekiq'
+
+  namespace :madmin do
+  end
+end
+
+  resources :notifications, only: [:index]
+  resources :announcements, only: [:index]
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
