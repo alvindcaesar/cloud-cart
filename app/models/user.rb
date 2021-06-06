16c9ac4d8e48a91f6f013afb742f5a6b865cd987
alvindcaesar <me@alvindcaesar.com>
2021-06-06 17:55:15 +0800

    Fix merge conflicts

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-has_person_name
-has_many :products
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-end
