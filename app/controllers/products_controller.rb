faccbdc192dcfc597a5705bdf40c45d47799a432
alvindcaesar <me@alvindcaesar.com>
2021-06-06 17:21:30 +0800

    Commits for 'feature/create-product' branch

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,44 +1,57 @@
 class ProductsController < ApplicationController
-before_action :set_product, only: %i[ show edit update destroy ]
-before_action :authenticate_user!
+  before_action :set_product, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
-    def index
-        @products = Product.all
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+  end
+
+  def create
+    @product = current_user.products.build(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: "Product was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
     end
+  end
 
-    def new
-        @product = Product.new
+  def update
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to @product, notice: "Product was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
     end
+  end
 
-    def create
-        @product = Product.new(product_params)
-        if @product.save
-            redirect_to @product, notice: "Your product is successfully created."
-        else
-            render :new, alert: "Product was not created. Please try again."
-        end
+  def destroy
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to products_url, notice: "Product was successfully destroyed." }
     end
+  end
 
-    def update
-        if @product.update(product_params)
-            redirect_to @product, notice: "Product was successfully updated."
-        else
-            render :edit, status: :unprocessable_entity
-        end
-    end
-
-    def destroy
-        @product.destroy
-        redirect_to products_url, notice: "Product was successfully deleted."
-    end
-
-    private
-
+  private
     def set_product
-        @product = Product.find(params[:id])
+      @product = Product.find(params[:id])
     end
 
     def product_params
-        params.require(:product).permit(:product_name, :sku, :price, :description)
+      params.require(:product).permit( :product_photo, :product_name, :price, :sku, :description )
     end
 end
