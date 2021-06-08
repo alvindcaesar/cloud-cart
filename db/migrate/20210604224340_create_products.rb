49828578327a4e0d3c25dacb8edbb8874ab2572d
alvindcaesar <me@alvindcaesar.com>
2021-06-09 01:34:19 +0800

    Commits for 'feature/create-orders' branch

--- db/migrate/20210604224340_create_products.rb
+++ db/migrate/20210604224340_create_products.rb
@@ -1,10 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
-      t.string :product_name
-      t.integer :price
+      t.string :product_name, default: ''
+      t.decimal :price, default: 0.0
       t.string :sku
-      t.string :description
+      t.string :description, default: ''
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+    name: "Test Account",
+    email: "test@gmail.com",
+    password: "password",
+    password_confirmation: "password"
+)
+
+10.times do |_i|
+    Product.create!(
+        product_name: "Super Product",
+        description: "Lorem ipsum super product that being used as a template",
+        price: 11.99,
+        sku: "SP-0001"
+    )
+end
